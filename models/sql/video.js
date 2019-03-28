f3f3eba3fa811fae1bd604dad229a71d0fe36982
Yuval Assa <assa.yuval107@gmail.com>
2019-03-28 15:22:58 +0200

    feat: create schemas

--- models/sql/video.js
+++ models/sql/video.js
@@ -1,16 +1,48 @@
 const Sequelize = require('sequelize');
 
-export default Video = sequelize.define('wp_hdflvvideoshare', {
-    // attributes
+const VideoSchema = {
     vid: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         primaryKey: true,
         allowNull: false,
     },
-    lastName: {
-        type: Sequelize.STRING
-        // allowNull defaults to true
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    name: {
+        type: Sequelize.TEXT
+    },
+    file: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+    },
+    image: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+    },
+    slug: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+    },
+    hitcount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    post_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    source: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    publish_procedure: {
+        type: Sequelize.INTEGER,
+    },
+    shos: {
+        type: Sequelize.INTEGER,
     }
-}, {
-        freezeTableName: true
-    });
+};
+
+module.exports = VideoSchema;

066a78f13f43c15e730d8eb6f7bd3a2100dc67e2
Yuval Assa <assa.yuval107@gmail.com>
2019-03-28 11:11:03 +0200

    refactor: dissable default timestamps on table

--- migrate.js
+++ migrate.js
@@ -1,19 +1,24 @@
 import videoSqlScheme from './models/sql/video'
+const connectionURI = "mysql://user:pass@example.com:port/dbname";
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+const sequelize = new Sequelize(connectionURI, {
+    define: {
+        timestamps: false,
+
+    }
+});
 
 try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-} catch(err) {
+} catch (err) {
     console.error('Unable to connect to the database:', err);
 }
 
-const SQL_video = sequelize.define('wp_hdflvvideoshare',videoSqlScheme)
-const SQL_video2category = sequelize.define('wp_hdflvvideoshare_med2play',)
-const SQL_category = sequelize.define('wp_hdflvvideoshare_playlist',);
-const SQL_tags = sequelize.define('wp_hdflvvideoshare_tags',);
-
-
+const SQL_video = sequelize.define('wp_hdflvvideoshare', videoSqlScheme)
+const SQL_video2category = sequelize.define('wp_hdflvvideoshare_med2play')
+const SQL_category = sequelize.define('wp_hdflvvideoshare_playlist');
+const SQL_tags = sequelize.define('wp_hdflvvideoshare_tags');
 
 sequelize.close()

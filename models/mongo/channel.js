c132b96efaef6f870648e6e298441e0f82713d9b
SirSaar <saar643@gmail.com>
2019-03-28 10:58:09 +0200

    feat: add channel model

---
+++ models/mongo/channel.js
@@ -0,0 +1,37 @@
+import * as mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema(
+    {
+        user: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        isProfile: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        versionKey: false,
+        autoIndex: false,
+        timestamps: true,
+        id: true,
+        toJSON: {
+            virtuals: true,
+            transform: (doc, ret) => {
+                delete ret._id;
+            },
+        },
+    });
+
+channelSchema.index({ name: 1, user: -1 });
+
+export const ChannelModel = mongoose.model('Channel', channelSchema);
